a8de3230f579afd2d94c8359d4077d0dd98aabdd
khai21112002 <113539072+khai21112002@users.noreply.github.com>
2023-04-13 14:02:26 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/testgit2/MainActivity.java
+++ app/src/main/java/com/example/testgit2/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Im from Pullgit");
+        System.out.println("Im from Pullgit");
     }
 }
