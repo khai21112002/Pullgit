6cdaa1dfb6c9144354527f498e6a06ce01111480
Amin <khai21112002@gmail.com>
2023-04-13 15:31:13 +0700

    initial message

--- app/src/main/java/com/example/testgit2/MainActivity.java
+++ app/src/main/java/com/example/testgit2/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("Im from Pullgit");
         System.out.println("Im from Pullgit");
+        System.out.println("Commit");
     }
 }
